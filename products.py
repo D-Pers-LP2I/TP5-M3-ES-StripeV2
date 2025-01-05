85466c41de13f10a3a2be9b1b54afabd47be938e
D-Pers-LP2I <daniel.pers@lp2i-poitiers.fr>
2025-01-06 00:32:55 +0100

    TP5 M3 ES V2 06_01_25

---
+++ products.py
@@ -0,0 +1,6 @@
+# Liste des produits disponibles
+# Les prix sont exprimés en centimes pour Stripe.
+PRODUCTS = [
+    {"id": 1, "name": "Produit 1", "price": 1400},  # 14,00 €
+    {"id": 2, "name": "Produit 2", "price": 2100},  # 21,00 €
+]
